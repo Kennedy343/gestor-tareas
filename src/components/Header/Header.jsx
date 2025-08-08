2f3642cfcccfbe1ba0c91a6dd3c7d80381fdabe6
Kennedy343 <91098373+Kennedy343@users.noreply.github.com>
2025-08-07 18:55:32 -0600

    feat: Agregacion componente header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-indigo-600 text-white px-6 py-4 shadow-md flex justify-between items-center">
+      <h1 className="text-2xl font-bold tracking-wide">Kanban App</h1>
+    </header>
+  );
+};
+
+export default Header;
